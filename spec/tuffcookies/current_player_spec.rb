4e80902d88ab367747409db949b5ca1cd7f7a361
Seth Geyer <seth.geyer@gmail.com>
2013-10-07 13:38:55 -0600

    re-write of current_player spec

--- spec/tuffcookies/current_player_spec.rb
+++ spec/tuffcookies/current_player_spec.rb
@@ -11,33 +11,33 @@ module TuffCookie
           game.dealer_flips_card(8) 
           
       end
-      context "when the player's 'guess' is 'correct'" do
-        it "should stay with the current player" do
-          game.guess('h', 4)
-          game.players_turn.should == "Seth"
-        end       
-      end   
-      context "when the player's 'guess' is 'swept'" do
-        it "should move to next player" do
-          game.guess('s', 4)
-          game.players_turn.should == "George" #game.players[game.players.index(game.players_turn)]
-          #game.players_turn.should == CurrentPlayer.new(game.players_turn).player_name
-        end       
-      end    
-      context "when the player's 'guess' is 'wrong'" do
-        it "should move to next player" do
-          game.guess('l', 4)
-          game.players_turn.should == "George" #game.players[game.players.index(game.players_turn)]
-          #game.players_turn.should == CurrentPlayer.new(game.players_turn).player_name
-        end       
-      end    
-      context "when the player's 'guess' is 'same'" do
-        it "should move to next player" do
-          game.guess('l', 4)
-          game.players_turn.should == "George" #game.players[game.players.index(game.players_turn)]
-          #game.players_turn.should == CurrentPlayer.new(game.players_turn).player_name
-        end       
-      end    
+  #     context "when the player's 'guess' is 'correct'" do
+#         it "should stay with the current player" do
+#           game.guess('h', 4)
+#           game.players_turn.should == "Seth"
+#         end       
+#       end   
+#       context "when the player's 'guess' is 'swept'" do
+#         it "should move to next player" do
+#           game.guess('s', 4)
+#           game.players_turn.should == "George" #game.players[game.players.index(game.players_turn)]
+#           #game.players_turn.should == CurrentPlayer.new(game.players_turn).player_name
+#         end       
+#       end    
+#       context "when the player's 'guess' is 'wrong'" do
+#         it "should move to next player" do
+#           game.guess('l', 4)
+#           game.players_turn.should == "George" #game.players[game.players.index(game.players_turn)]
+#           #game.players_turn.should == CurrentPlayer.new(game.players_turn).player_name
+#         end       
+#       end    
+#       context "when the player's 'guess' is 'same'" do
+#         it "should move to next player" do
+#           game.guess('l', 4)
+#           game.players_turn.should == "George" #game.players[game.players.index(game.players_turn)]
+#           #game.players_turn.should == CurrentPlayer.new(game.players_turn).player_name
+#         end       
+#       end    
 
 
 
